0fc764cc5350f530a4c52628740275379a736f19
WinslowEric <i@winsloweric.cn>
2022-03-27 03:07:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //Simply run this in your browser(with login session of course)
+main();
+async function main(){
+  var CSRFToken = document.getElementsByName("csrf-token")[0].content;
+}
+async function fetchAllPastPost(){
+
+}
+
+async function deletePost(){
+
+}
