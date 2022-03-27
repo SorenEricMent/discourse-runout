2a06d461d5f225f264ef5d384e385dca86c5ef19
WinslowEric <i@winsloweric.cn>
2022-03-27 11:32:39 +0800

    Update browser-side.js

--- browser-side.js
+++ browser-side.js
@@ -10,16 +10,21 @@ async function main(){
   script.src = "https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.min.js";
   document.body.appendChild(script); //注入jQuery
   
+  var postIDList = [];
   var CSRFToken = document.getElementsByName("csrf-token")[0].content; //获取CSRF凭证
   
 }
 async function fetchPostIteration(currentOffset){
   
     //https://limelight.moe/user_actions.json?offset=21&username=winslow
-  await $.post(
+  try{
+    let requestData = await $.post(
     "https://limelight.moe/user_actions.json?offset="+currentOffset+"&username="+config.username
-  
-  );
+    ); //Maximum fetch per time: 30, out of offset = less
+  }catch{
+    console.warn("Error encountered when fetching OFFSET" + currenOffset + ",skipping...");
+  }
+
 }
 
 async function postRemoveLoop(){
