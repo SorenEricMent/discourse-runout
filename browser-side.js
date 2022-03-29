0aa165e31a99fa9d010cc036f833bfad915f805e
WinslowEric <i@winsloweric.cn>
2022-03-29 20:23:09 +0800

    Update browser-side.js

--- browser-side.js
+++ browser-side.js
@@ -3,62 +3,59 @@ main();
 
 async function main(){
   retryThreshold = 2;
-  unwrappedUsername = $("#current-user")[0].childNodes[0].href.split("/")
-  const config = {
-    "username": unwrappedUsername[unwrappedUsername.length-1], //Change this to your username!
+  wrappedUsername = $("#current-user")[0].childNodes[0].href.split("/")
+  config = {
+    "username": wrappedUsername[wrappedUsername.length-1],
     "host": window.location.protocol + "//" + window.location.host
   }
   
   var postIDList = [];
   var CSRFToken = document.getElementsByName("csrf-token")[0].content; //获取CSRF凭证
-  
+  removePostIteration(0,CSRFToken,1);
 }
-async function fetchPostIteration(currentOffset){
-  
-    //https://limelight.moe/user_actions.json?offset=21&username=winslow
-  try{
-    removeRequestState = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    let requestData = await $.post(
+async function removePostIteration(currentOffset,CSRFToken,TTL){
+    let requestData = await $.get(
     config.host + "/user_actions.json?offset="+currentOffset+"&username="+config.username
-    ); //Maximum fetch per time: 30, out of offset = less
-    let currentFetchedList = requestData.responseJSON;
-    for(var i=0;i<currentFetchedList.length;i++){
-    
-    }
-    if(currentFetchedList.length == 30){
-      fetchPostIteration(currentOffset + 30);
-    }else{
-      console.log("Task finished at offset" + currentOffset);
-    }
-  }catch(){
-    console.warn("Error encountered when fetching OFFSET" + currenOffset + ",skipping...");
-  }
-
+    ); 
+    let currentFetchedList = requestData.user_actions;
+    loopCounter = 0;
+    successCount = 0;
+    failedCount = 0;
+    requestLoop = setInterval(async function(currentOffset,currentFetchedList,CSRFToken,TTL){
+      let isSuccess = true;
+      try{
+        await removePost(loopCounter,currentFetchedList[loopCounter].post_id,CSRFToken);
+      }catch{
+        isSuccess = false;
+      }
+      
+      if(isSuccess){
+        successCount++;
+      }else{
+        failedCount++;
+      }
+      
+      if(failedCount == (currentFetchedList.length){
+          console.log("All remove operations failed in this Iteration, check if something happened. Script halted.");
+          clearInterval(requestLoop);
+      }else{
+      if(currentFetchedList.length != 30){
+        if((failedCount + successCount) == currentFetchedList.length){
+         console.log("Iteration #"+TTL+" finished,"+successCount+" post removed," + failedCount + " failed.");
+        }else if((failedCount + successCount) == 30){
+          console.log(("Task finished at offset" +currentOffset + ", iteration #"+ TTL + "," +successCount+" post removed," + failedCount + " failed.");
+        }                       
+      }
+    },2000)
 }
 
-async function postRemoveLoop(){
-  
-}
-
-function removePost(requestIndex,postID,csrf,timeToLive){
+async function removePost(requestIndex,postID,csrf){
   let requestData = {
     "_method":"delete",
     "headers": {
       "x-csrf-token": CSRFToken
     },
-    "creaditals": "include"
+    "credentials": "include"
   };
-  try{
-    $.post(config.host + "/posts/"+postID,requestData,function callback(requestIndex,timeToLive){
-      removeRequestState[requestIndex] = 1;
-    });
-  }catch(requestIndex,postID,csrf,timeToLive){
-    if(timeToLive <= retryThreshold){
-      console.warn("Request x"+requestIndex+" to remove" + postID + "failed, TTL pass, retrying...");
-      removePost(requestIndex,postID,csrf,timeToLive+1)
-    }else{
-      console.error("Request x"+requestIndex+" to remove" + postID + " all retry failed!");
-      removeRequestState[requestIndex] = -1;
-    }
-  }
+  return await $.post(config.host + "/posts/"+postID,requestData);
 }
